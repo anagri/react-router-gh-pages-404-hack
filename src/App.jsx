6018e4ba503196f2166dd26bb7b148688fa99688
anagri <127566+anagri@users.noreply.github.com>
2025-10-04 09:12:04 +0530

    [Amir] integrated oauth callback

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,22 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navigation from './components/Navigation';
+import Header from './components/Header';
 import Home from './pages/Home';
 import About from './pages/About';
+import OAuthCallback from './pages/OAuthCallback';
 import './App.css';
 
 function App() {
   return (
     <Router basename="/react-router-gh-pages-404-hack">
-      <div>
-        <Navigation />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
+      <div className="app">
+        <Header />
+        <main className="main-content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/oauth/callback" element={<OAuthCallback />} />
+          </Routes>
+        </main>
       </div>
     </Router>
   );
